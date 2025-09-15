e78357cf1645254f5de6b3678009c4cf9a0e374b
Tomoya-Sonok <50537591+Tomoya-Sonok@users.noreply.github.com>
2025-09-15 14:01:01 +0900

    chore: 型しか使わないimportはtypeをつける

--- frontend/lib/server/trpc/router/_app.ts
+++ frontend/lib/server/trpc/router/_app.ts
@@ -1,9 +1,9 @@
-import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { router } from "@/server/trpc";
 import { trainingTagsRouter } from "./trainingTags";
 
 export const appRouter = router({
-  trainingTags: trainingTagsRouter,
+	trainingTags: trainingTagsRouter,
 });
 
 export type AppRouter = typeof appRouter;
--- frontend/server/trpc/error.ts
+++ frontend/server/trpc/error.ts
@@ -1,30 +1,30 @@
-import { TRPC_ERROR_CODE_KEY } from "@trpc/server";
-import { AxiosError } from "axios";
-import { StatusCodes } from "http-status-codes";
+import type { TRPC_ERROR_CODE_KEY } from "@trpc/server";
+import type { AxiosError } from "axios";
+import type { StatusCodes } from "http-status-codes";
 
 export function mapTRPCErrorCodeKeyFromStatusCode(
-  statusCode: StatusCodes,
+	statusCode: StatusCodes,
 ): TRPC_ERROR_CODE_KEY {
-  switch (statusCode) {
-    case 400:
-      return "BAD_REQUEST";
-    case 401:
-      return "UNAUTHORIZED";
-    case 403:
-      return "FORBIDDEN";
-    case 404:
-      return "NOT_FOUND";
-    case 429:
-      return "TOO_MANY_REQUESTS";
-    default:
-      return "INTERNAL_SERVER_ERROR";
-  }
+	switch (statusCode) {
+		case 400:
+			return "BAD_REQUEST";
+		case 401:
+			return "UNAUTHORIZED";
+		case 403:
+			return "FORBIDDEN";
+		case 404:
+			return "NOT_FOUND";
+		case 429:
+			return "TOO_MANY_REQUESTS";
+		default:
+			return "INTERNAL_SERVER_ERROR";
+	}
 }
 
 export function mapAxiosErrorToTRPCError(label: string, err: AxiosError) {
-  return {
-    code: mapTRPCErrorCodeKeyFromStatusCode(err.response?.status || 500),
-    message: `${err.response?.statusText || "Internal Server Error"}: ${label}`,
-    cause: err,
-  };
+	return {
+		code: mapTRPCErrorCodeKeyFromStatusCode(err.response?.status || 500),
+		message: `${err.response?.statusText || "Internal Server Error"}: ${label}`,
+		cause: err,
+	};
 }
